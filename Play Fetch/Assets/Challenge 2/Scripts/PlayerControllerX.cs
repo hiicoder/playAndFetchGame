788a9ccd17f330bef09ac06df41928de314adf21
Himanshu kumar <49545079+hiicoder@users.noreply.github.com>
2022-03-02 22:10:08 +0530

    Add game

---
+++ Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float lastCall = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - lastCall > 0.5f )
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastCall = Time.time;
+        }
+    }
+}
